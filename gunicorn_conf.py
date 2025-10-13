5a624ee843007218451e4e863a23cce3dbd2b39e
azam2015sql-cloud <azam2015sql@gmail.com>
2025-10-13 13:24:25 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -2,7 +2,7 @@
 # Tune Gunicorn for Render: increase timeout and set modest number of workers
 workers = 2
 threads = 2
-timeout = 120
+timeout = 90
 keepalive = 5
 bind = "0.0.0.0:%s" % (os.environ.get("PORT", "10000"))
 loglevel = "info"
